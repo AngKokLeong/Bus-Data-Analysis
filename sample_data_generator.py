5b77de9f854d020fec01efeee4d4cab9243935a3
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-07-28 13:22:51 +0000

    added method to generate confidence interval for z test

--- sample_data_generator.py
+++ sample_data_generator.py
@@ -1,20 +1,35 @@
 import numpy, pandas, scipy, math
 
-def generate_confidence_interval_for_t_test(significance_level: float, sample_mean: float, sample_standard_deviation: float, data_source: pandas.Series) -> dict:
+def generate_confidence_interval_for_t_test_with_unknown_sigma(significance_level: float, sample_mean: float, sample_standard_deviation: float, data_source: pandas.Series) -> dict:
     number_of_samples: int = len(data_source)
     degree_of_freedom: int = number_of_samples - 1
     critical_value: float = scipy.stats.t.ppf(significance_level, degree_of_freedom)
 
     # Using the t-test and assume population standard deviation is unknown
 
-    upper_bound_confidence_interval:float = sample_mean - (critical_value * (sample_standard_deviation / math.sqrt(number_of_samples)))
-    lower_bound_confidence_interval:float = sample_mean + (critical_value * (sample_standard_deviation / math.sqrt(number_of_samples)))
+    lower_bound_confidence_interval:float = sample_mean - (critical_value * (sample_standard_deviation / math.sqrt(number_of_samples)))
+    upper_bound_confidence_interval:float = sample_mean + (critical_value * (sample_standard_deviation / math.sqrt(number_of_samples)))
 
     return {
             "Sample_Mean": sample_mean, 
             "Lower Bound Confidence Interval": lower_bound_confidence_interval, 
             "Upper Bound Confidence Interval": upper_bound_confidence_interval
             }
+
+def generate_confidence_interval_for_z_test_with_known_sigma(significance_level: float, sample_mean: float, standard_deviation: float, data_source: pandas.Series) -> dict:
+    number_of_samples: int = len(data_source)
+    degree_of_freedom: int = number_of_samples - 1
+    z_critical_value: float = scipy.stats.norm.ppf(1-significance_level)
+
+    lower_bound_confidence_interval:float = sample_mean - (z_critical_value * (standard_deviation / math.sqrt(number_of_samples)))
+    upper_bound_confidence_interval :float = sample_mean + (z_critical_value * (standard_deviation / math.sqrt(number_of_samples)))
+
+    return {
+            "Sample_Mean": sample_mean, 
+            "Lower Bound Confidence Interval": lower_bound_confidence_interval, 
+            "Upper Bound Confidence Interval": upper_bound_confidence_interval
+            }
+
 
 
 def systematic_sampling(dataframe: pandas.DataFrame, every_nth_member: int) -> pandas.DataFrame:
